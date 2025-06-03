013c532b56e4ed3ca5847355313a8361b0b1bb03
Sujal <0987sujals@gmail.com>
2025-06-03 04:19:20 +0000

    Full redis based market data service

--- app/models/base.py
+++ app/models/base.py
@@ -1,3 +1,16 @@
+"""
+Base models for the pure Redis service
+"""
+
+from enum import Enum
+
+class BrokerName(Enum):
+    ANGEL_ONE = "ANGEL_ONE"
+    
+class TimeFrame(Enum):
+    SECOND_1 = "1s"
+    MINUTE_1 = "1m"
+
 from sqlalchemy import (
     Boolean,
     Column,
