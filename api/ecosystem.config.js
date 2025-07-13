f1d2d0c08c5c181a1317fc1d5feb66f0e4084355
Jai Bhardwaj <0987sujals@gmail.com>
2025-07-13 19:29:30 +0530

    fixed

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: 'uvicorn',
       args: 'api.main:app --host 0.0.0.0 --port 8000 --reload',
       interpreter: 'python3',
+      cwd: '../',
       watch: false,
       env: {
         "PYTHONPATH": "."
